a822eb92aca0c623a581effe9e02be712c39aa33
drewb10 <drewmaster03@gmail.com>
2025-06-23 11:29:04 -0400

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 import { Platform } from 'react-native';
-import { Home, User, Camera, Trophy } from 'lucide-react-native';
+import { Chrome as Home, User, Camera, Trophy } from 'lucide-react-native';
 import { colors } from '@/constants/colors';
 import { useThemeStore } from '@/stores/themeStore';
 
